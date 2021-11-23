3c12998764de0f856eadb47098c59f35e4dbdfcc
Deki97 <dejan97@libero.it>
2021-11-23 17:01:05 +0100

    select options

--- js/script.js
+++ js/script.js
@@ -5,3 +5,62 @@
 // con difficoltà 3 => tra 1 e 49
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
+const myGrid = document.querySelector('.game-grid');
+// console.log(myGrid);
+
+const playBtn = document.querySelector('.play-button').addEventListener('click', optionSelected);
+
+// Griglia generata se l'utente sceglie la Option Easy dalla Select (100 numeri -> da 1 a 100)
+function optionSelected() {
+    if(document.getElementById('select-level').value == "easy") {
+        myGrid.innerHTML = '';
+        for(let i = 1; i <= 100; i++) {
+            let thisElement = i;
+        
+            const thisSquare = document.createElement('div');
+            thisSquare.classList.add('square');
+            thisSquare.innerHTML = `${thisElement}`;
+            
+            thisSquare.style.width = 'calc(100% / 10);';
+            thisSquare.style.height = 'calc(100% / 10);';
+        
+            myGrid.appendChild(thisSquare);
+        }
+    } 
+    else if(document.getElementById('select-level').value == "normal") {
+        myGrid.innerHTML = '';
+        for(let i = 1; i <= 81; i++) {
+            let thisElement = i;
+        
+            const thisSquare = document.createElement('div');
+            thisSquare.classList.add('square');
+            thisSquare.innerHTML = `${thisElement}`;
+            
+            thisSquare.style.width = 'calc(100% / 9);';
+            thisSquare.style.height = 'calc(100% / 9);';
+        
+            myGrid.appendChild(thisSquare);
+        }
+    } 
+    else if(document.getElementById('select-level').value == "hard") {
+        myGrid.innerHTML = '';
+        for(let i = 1; i <= 49; i++) {
+            let thisElement = i;
+        
+            const thisSquare = document.createElement('div');
+            thisSquare.classList.add('square');
+            thisSquare.innerHTML = `${thisElement}`;
+            
+            thisSquare.style.width = 'calc(100% / 7);';
+            thisSquare.style.height = 'calc(100% / 7);';
+        
+            myGrid.appendChild(thisSquare);
+        }
+    }
+}
+
+
+// Griglia generata se l'utente sceglie la Option Normal dalla Select (81 numeri -> da 1 a 81)
+
+
+// Griglia generata se l'utente sceglie la Option Hard dalla Select (49 numeri -> da 1 a 49)
